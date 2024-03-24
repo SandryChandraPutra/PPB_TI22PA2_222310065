9d47f84eaa6c3bda006fa5a66b7aca77b8cd4648
SandryChandraPutra <113377829+SandryChandraPutra@users.noreply.github.com>
2024-03-24 22:13:50 +0700

    Create App.js

---
+++ Pertemuan 1/App.js
@@ -0,0 +1,19 @@
+import { StyleSheet, Text, View } from 'react-native';
+import Profile from './components/Profile';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Profile/>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
